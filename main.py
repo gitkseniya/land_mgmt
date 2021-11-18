97d53a212f0aac0874e76c68438e5c4ea053a233
Kseniya K <kseniya.kudelich@gmail.com>
2021-11-18 10:42:14 -0700

    Add missing templates

--- main.py
+++ main.py
@@ -9,17 +9,32 @@ app = Flask(__name__)
 api = Api(app)
 
 
-
 api.add_resource(Owners, '/api/owners')
 api.add_resource(Units, '/api/units')
 api.add_resource(OwnersByUnit, '/api/units/<int:id>/owners')
 api.add_resource(CreateOwner, '/api/create_owner')
+api.add_resource(CreateUnit, '/api/create_unit')
+api.add_resource(CreateUnitOwner, '/api/create_unit_owner')
+api.add_resource(DeleteUnitOwner, '/api/delete_unit_owner')
+
 
 @app.route("/owners")
-def index():
+def owner_index():
     data = requests.get("http://localhost:5000/api/owners")
 
-    return render_template("index.html", message=data.json());
+    return render_template("owners/index.html", message=data.json());
+
+
+@app.route("/units")
+def unit_index():
+    data = requests.get("http://localhost:5000/api/units")
+
+    return render_template("units/index.html", message=data.json());
+
+
+@app.route("/")
+def test():
+    return render_template("/welcome/index.html");
 
 
 if __name__ == '__main__':
