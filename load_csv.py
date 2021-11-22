f0a679ddee7ced7aae1be9016c231b2ff413456c
Kseniya K <kseniya.kudelich@gmail.com>
2021-11-22 14:00:37 -0700

    Add find by sql csv import

--- load_csv.py
+++ load_csv.py
@@ -1,25 +1,25 @@
 from google.cloud import bigquery, storage
 
 client = bigquery.Client()
-# client2 = storage.Client()
-# bucket = client2.get_bucket('tp_land_data')
-# breakpoint()
 
-table_id = "landmanagementservice.land_deal_info.owners"
+table_id = "landmanagementservice.land_deal_info.unit_owners"
 
 job_config = bigquery.LoadJobConfig(
     schema=[
-        bigquery.SchemaField("full_name", "STRING"),
-        bigquery.SchemaField("address", "STRING"),
-        bigquery.SchemaField("county_state_zip", "STRING"),
-        bigquery.SchemaField("phone_no", "STRING")
+        bigquery.SchemaField("owner_id", "STRING"),
+        bigquery.SchemaField("interest_type", "STRING"),
+        bigquery.SchemaField("current_owner", "BOOLEAN"),
+        bigquery.SchemaField("comments", "STRING"),
+        bigquery.SchemaField("vesting_docs", "STRING"),
     ],
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    source_format=bigquery.SourceFormat.CSV,
     skip_leading_rows=1,
     allow_quoted_newlines = True
     # The source format defaults to CSV, so the line below is optional.
     # source_format=bigquery.SourceFormat.CSV,
 )
-uri = "https://storage.cloud.google.com/tp_land_data/bq_owners.csv"
+uri = "https://storage.cloud.google.com/tp_land_data/bq_units2.csv"
 
 load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
