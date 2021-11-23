b79384bc568fbdbb57a2dfc283dfe10a55f54b9b
Kseniya K <kseniya.kudelich@gmail.com>
2021-11-23 15:47:00 -0700

    Add owners index test

---
+++ src/test_owners.py
@@ -0,0 +1,32 @@
+from .routes import Owners
+import pytest
+from flask import Flask
+from flask_restful import Api
+
+@pytest.fixture
+def Resource():
+    route = Owners()
+    return route
+
+
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    api = Api(app)
+    api.add_resource(Owners, "/api/owners")
+
+    return app.test_client()
+
+
+def test_owners_index(app):
+
+    response = app.get("/api/owners").json
+
+    assert isinstance(response, list)
+    assert len(response) >= 100
+
+    assert isinstance(response[0]["full_name"], str)
+    assert isinstance(response[0]["address"], str)
+    assert isinstance(response[0]["id"], str)
+    assert isinstance(response[0]["county_state_zip"], str)
+    assert isinstance(response[0]["phone_no"], str)
